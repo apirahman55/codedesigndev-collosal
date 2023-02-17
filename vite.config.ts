d1551a68cfc4aaa6891c03b9fd4988c5738255fa
apirahman55 <apirahman55@gmail.com>
2023-02-17 13:46:32 -0800

    setup tailwind atomic architecture for ui design

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'assets': path.resolve(__dirname, './src/assets'),
+      'pages': path.resolve(__dirname, './src/pages'),
+      'components': path.resolve(__dirname, './src/components'),
+      'atoms': path.resolve(__dirname, './src/components/atoms'),
+      'hoc': path.resolve(__dirname, './src/components/hoc'),
+      'molecules': path.resolve(__dirname, './src/components/molecules'),
+      'organisms': path.resolve(__dirname, './src/components/organisms'),
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 })
